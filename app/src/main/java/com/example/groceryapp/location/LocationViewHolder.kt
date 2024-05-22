9e8a2c30869245dea9fd1b99efc7e44c592703b5
ashmitaofficial <ashmitashrivastava123@gmail.com>
2024-05-22 07:43:54 +0530

    location bug fix on shop fragment

--- app/src/main/java/com/example/groceryapp/location/LocationViewHolder.kt
+++ app/src/main/java/com/example/groceryapp/location/LocationViewHolder.kt
@@ -6,11 +6,23 @@ import com.example.groceryapp.R
 import com.example.groceryapp.base.BaseViewHolder
 import com.example.groceryapp.base.DynamicItem
 
-class LocationViewHolder(itemView: View):BaseViewHolder(itemView) {
+class LocationViewHolder(itemView: View) : BaseViewHolder(itemView) {
     override fun bind(item: DynamicItem) {
-        val data = item.data as String
-        val textView= itemView.findViewById<TextView>(R.id.location_txt)
-        textView.text=data.substring(0,6)+"..."
+        var data: String? = null
+        var length =0
+        item.data?.let {
+            data = it as String
+            length=it.length
+        }
+
+        val textView = itemView.findViewById<TextView>(R.id.location_txt)
+
+        textView.text = (if (length >= 6) {
+            data?.substring(0, 6) + "..."
+        } else {
+            data
+        }).toString()
+
 
     }
 }
